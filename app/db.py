ff647a03b6a5a9ca8a769b902cc42071c86f8134
Vadim <75739522+vdsmirnovv@users.noreply.github.com>
2025-04-10 05:11:02 +0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -3,15 +3,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Строка подключения к базе данных
+
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:uftHgAOGeXIkjAIRjkVuBwjqvSETmdzM@crossover.proxy.rlwy.net:12938/railway"
 
-# Создаём движок подключения (без параметра check_same_thread)
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-# Создаём сессию для работы с базой данных
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Базовый класс для всех моделей
+
 Base = declarative_base()
 
